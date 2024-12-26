0b10d691073fdcac4f4877c2a55c48786b5daa9f
houdabelhad1 <diyaenour75@gmail.com>
2024-12-26 23:25:52 +0100

    1

---
+++ src/main/java/com/hbdev/healthsphere/HealthSphereApplication.java
@@ -0,0 +1,13 @@
+package com.hbdev.healthsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hbdev/healthsphere/HealthSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbdev.healthsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
